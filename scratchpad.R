82eb4bd280f08ea94ed30ee4311d6a6a7ddb4331
suspicious-cow <zain_naboulsi@msn.com>
2023-05-23 15:01:22 -0500

    Spit groups into member and casual. started basic metrics for both groups

--- scratchpad.R
+++ scratchpad.R
@@ -36,6 +36,8 @@ if(file.exists("combined_data_df.rds") && !exists("combined_data_df")) {
     write_rds(combined_data_df, "combined_data_df.rds")
 }
 
+# =====================================================
+
 # examine the structure of the data
 str(combined_data_df)
 
@@ -56,13 +58,26 @@ num_duplicates <- sum(duplicates)
 print(num_duplicates)
 
 
+# ======================================================
 
-# calculate the total ride time in minutes
+# calculate the total ride time in minutes for each group
 combined_data_df$ride_length <- 
     round((combined_data_df$ended_at - combined_data_df$started_at) / 60, 2)
 
 # Change the units attribute to "minutes"
 attr(combined_data_df$ride_length, "units") <- "minutes"
+
+# Get rid of outliers by only accepting ride times that are less than two days 
+# and greater than zero
+combined_data_df <- combined_data_df %>% filter(ride_length <= 2880 & 
+                                                    ride_length > 5) 
+
+
+# Add a new column for the date without the time
+combined_data_df$started_date <- as.Date(combined_data_df$started_at)
+
+
+
 
 
 # Create the 'day_of_week_start' and 'day_of_week_end' columns
@@ -73,28 +88,39 @@ combined_data_df$day_of_week_end <- wday(combined_data_df$ended_at, label = TRUE
 combined_data_df$day_of_week_start <- as.factor(combined_data_df$day_of_week_start)
 combined_data_df$day_of_week_end <- as.factor(combined_data_df$day_of_week_end)
 
-# ride_length metrics
-ride_length_mean <- mean(as.numeric(combined_data_df$ride_length))
-ride_length_max <- max(as.numeric(combined_data_df$ride_length))
-ride_length_min <- min(as.numeric(combined_data_df$ride_length))
-
-cat("The average rental time was", round(ride_length_mean, 2), 
-    "minutes long.\n The longest rental time was", ride_length_max,"minutes\n",
-    "and the shortest rental time was", ride_length_min, "minutes.")
-
-str(combined_data_df)
 
 
 
-# Get rid of outliers by only accepting ride times that are less than two days 
-# and greater than zero
-combined_data_df <- combined_data_df %>% filter(ride_length <= 2880 & ride_length > 0) 
+# split our data into two groups: member and casual
+member_df <- combined_data_df %>% filter(member_casual == "member") 
+casual_df <- combined_data_df %>% filter(member_casual == "casual") 
+
+# ride_length metrics for both groups
+ride_length_mean_member <- mean(as.numeric(member_df$ride_length))
+ride_length_max_member <- max(as.numeric(member_df$ride_length))
+ride_length_min_member <- min(as.numeric(member_df$ride_length))
+
+ride_length_mean_casual <- mean(as.numeric(casual_df$ride_length))
+ride_length_max_casual <- max(as.numeric(casual_df$ride_length))
+ride_length_min_casual <- min(as.numeric(casual_df$ride_length))
+
+# Create a data frame
+ride_length_metrics <- data.frame(
+    Metric = c("Mean", "Max", "Min"),
+    Member = round(c(ride_length_mean_member, ride_length_max_member, ride_length_min_member), 2),
+    Casual = round(c(ride_length_mean_casual, ride_length_max_casual, ride_length_min_casual), 2)
+)
+
+# Print the data frame
+print(ride_length_metrics)
 
 
 
 
-# Add a new column for the date without the time
-combined_data_df$started_date <- as.Date(combined_data_df$started_at)
+
+
+
+
 
 # Group by date and count the number of rides each day
 daily_counts <- combined_data_df %>% group_by(started_date) %>% summarise(count = n())
@@ -113,7 +139,7 @@ ggplot(daily_counts, aes(x = started_date, y = count)) +
          y = "Number of Rentals", 
          title = "Daily Bike Rentals", 
          subtitle = "With trend line",
-         caption = "Source: Your Data Source") +
+         caption = "Source: https://divvybikes.com/system-data") +
     theme_minimal() +
     theme(plot.title = element_text(face = "bold", size = 20),
           plot.subtitle = element_text(face = "italic", size = 12),
