cb57036aa2378ed12eba7eb6bbfc79160ff6bb63
suspicious-cow <zain_naboulsi@msn.com>
2023-05-22 21:43:24 -0500

    Cleaned up the Prepare section and made ready to put in code chunks for cleaning the data

--- scratchpad.R
+++ scratchpad.R
@@ -3,7 +3,7 @@ set.seed(1337)
 
 
 # Set the names of the packages and libraries you want to install
-required_libraries <- c("tidyverse", "purrr", "knitr")
+required_libraries <- c("tidyverse", "purrr", "knitr", "skimr")
 
 # Install missing packages and load all required libraries
 for (lib in required_libraries) {
@@ -15,11 +15,11 @@ for (lib in required_libraries) {
 
 # load all the cycling data for analysis
 # our data runs from 04/2022 to 04/2023
-if(file.exists("combined_data_df.rds")) {
+if(file.exists("combined_data_df.rds") && !exists("combined_data_df")) {
     # read in our combined data
     combined_data_df <- readRDS("combined_data_df.rds")
     
-} else {
+} else if(!exists("combined_data_df")){
     
     # get file list
     file_list <- list.files(path = "SourceData/", pattern = "*.csv", full.names = TRUE)
@@ -37,8 +37,11 @@ if(file.exists("combined_data_df.rds")) {
 }
 
 
-rideable_unique_values <- levels(your_dataframe$member_casual)
-print(unique_values)
+member_casual_unique_values <- levels(combined_data_df$member_casual)
+print(member_casual_unique_values)
+
+rideable_unique_values <- levels(combined_data_df$rideable_type)
+print(rideable_unique_values)
 
 
 
